922fafec98aab3d581118c5419fe7c2fc3d93ff5
RyanLaiber <ryanlaiber@gmail.com>
2021-07-28 18:34:58 -0300

    implementa action assincrona e instala dependencia redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 // Importe o método applyMiddleware
-import { createStore } from 'redux';
-// Importe o redux-thunk
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-// Aplique o middleware
+const extension = window.devToolsExtension() || ((f) => f);
 const store = createStore(
   rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    extension,
+  )
 );
 
 export default store;
